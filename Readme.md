d18e4b26e7a523752579e361186d284bce0f1b65
kif <kifff.tn@gmail.com>
2025-04-22 00:17:26 +0700

    projrct sudah di tambahkan

---
+++ Readme.md
@@ -0,0 +1,35 @@
+# Air Quality Analysis with Python
+
+## Dataset
+
+[Air Quality](https://drive.google.com/file/d/1oog6Hd21g0OTGKvhrWNbpn5Khow2eOlD/view?usp=sharing)
+
+## Dashboard
+
+Link dashboard : [https://luzuqpxpvn6uicgutrk9r2.streamlit.app/](https://luzuqpxpvn6uicgutrk9r2.streamlit.app/)
+
+## Installation
+
+Clone Project
+
+```bash
+  git clone https://github.com/kifff/analisis_data_dg_python.git
+```
+
+Move to Project
+
+```bash
+  cd submission/
+```
+
+Run Streamlit
+
+```bash
+  streamlit run dashboard.py
+```
+
+## Authors
+
+- [@attmhd](https://github.com/kifff)
+
+- Linkedin : [attmhd](https://www.linkedin.com/in/kifatul-najmi-a089151b8/)
---
+++ dasboard/dasboard.py
@@ -0,0 +1,35 @@
+import streamlit as st 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("../data/main_data.csv")
+
+st.title("Dasboard peminjaman sepeda")
+
+kolom_opsi =['season', 'yr', 'mnth', 'holiday', 'weekday', 'workingday', 'weathersit',
+            'temp', 'atemp', 'hum', 'windspeed', 'casual', 'registered', 'cnt', 'hr']
+
+pilihan = st.selectbox("Pilih kolom untuk melihat visiual data :" , kolom_opsi)
+
+st.markdown("---")
+
+if df[pilihan].nunique() <= 20:
+    st.subheader(f"Jumlah data berdasarkan : {pilihan}")
+    fig, ax = plt.subplots(figsize=(8,5))
+    sns.barplot(x=pilihan , y='cnt',data=df,estimator=sum,ci=None,palette="Set2",ax=ax )
+    plt.xlabel(pilihan.capitalize())
+    plt.ylabel("Total peminjaman (cnt)")
+    st.pyplot(fig)
+
+else:
+    st.subheader(f"📊 Distribusi Nilai: {pilihan.capitalize()}")
+    fig, ax = plt.subplots(figsize=(8,5))
+    sns.histplot(df[pilihan], bins=30, kde=True, color='skyblue', ax=ax)
+    plt.xlabel(pilihan.capitalize())
+    plt.ylabel("Frekuensi")
+    st.pyplot(fig)    
+    
+    
+    
+    
