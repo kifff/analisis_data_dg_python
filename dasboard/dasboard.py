9d21ddb8e5095742d6556633ed814ecd6fea0576
kif <kifff.tn@gmail.com>
2025-05-02 23:52:53 +0700

    ubah dikit lagi hehehhhh

--- dasboard/dasboard.py
+++ dasboard/dasboard.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("main_data.csv")
+data = pd.read_csv("./main_data.csv")
 
 data['dteday'] = pd.to_datetime(data['dteday'])
 
